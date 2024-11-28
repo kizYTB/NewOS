811f1addfd30983d104d7a57362f177f451a9cfe
kiz___ <140178579+kizYTB@users.noreply.github.com>
2024-11-29 00:07:12 +0100

    install complet

--- TelOS-Install.lua
+++ TelOS-Install.lua
@@ -2,49 +2,40 @@ local run = shell.run
 
 local function Bootloader()
 	run("cd","/.bootloader/OS/")
-	run("wget","https://kiz-data.jtheberg.fr/CCMC/OS/sc/.NewOS/bootloaderconfigfile/NewOS.json")
+	run("wget","https://raw.githubusercontent.com/kizYTB/NewOS/refs/heads/main/.TelOS/bootloader_config_file/TelOS.json")
 end
 
 local function NewOS()
     run("cd","/")
-    run("mkdir",".NewOS")
+    run("mkdir",".TelOS")
 
-    run("cd","/.NewOS")
+    run("cd","/.TelOS")
 
     run("mkdir","Commands","Conf","debug","Prog","Shell","Up","pkg")
 	
-    run("wget","https://kiz-data.jtheberg.fr/CCMC/OS/sc/.NewOS/boot.lua","boot.lua")
+    run("wget","https://raw.githubusercontent.com/kizYTB/NewOS/refs/heads/main/.TelOS/boot.lua","boot.lua")
 end
 
 local function Components()
-    run("cd","/.NewOS")
+    run("cd","/.TelOS")
 	
     run("cd","Commands/")    
-    run("wget","https://kiz-data.jtheberg.fr/CCMC/OS/sc/.NewOS/Commands/help.lua","help.lua")
+    run("wget","https://raw.githubusercontent.com/kizYTB/NewOS/refs/heads/main/.TelOS/Commands/help.lua","help.lua")
 
-    run("cd","debug/")
-    run("wget","https://kiz-data.jtheberg.fr/CCMC/OS/sc/.NewOS/debug/","debug.lua")
-    run("mkdir","commands")
-    run("cd","commands")
-    run("wget","https://kiz-data.jtheberg.fr/CCMC/OS/sc/.NewOS/debug/commands/reset.lua","reset.lua")
+    run("cd","/.TelOS/Prog")
+    run("wget","https://raw.githubusercontent.com/kizYTB/NewOS/refs/heads/main/.TelOS/Prog/firewolf.lua","firewolf.lua")
+    run("wget","https://raw.githubusercontent.com/kizYTB/NewOS/refs/heads/main/.TelOS/Prog/ide.lua")
+    run("wget","https://raw.githubusercontent.com/kizYTB/NewOS/refs/heads/main/.TelOS/Prog/music.lua","music.lua")
 
-    run("cd","/.NewOS/Conf/")
-    run("wget","https://kiz-data.jtheberg.fr/CCMC/OS/sc/.NewOS/Conf/version.txt","version.txt")
-
-    run("cd","/.NewOS/Prog")
-    run("wget","https://kiz-data.jtheberg.fr/CCMC/OS/sc/.NewOS/Prog/firewolf.lua","firewolf.lua")
-    run("wget","https://kiz-data.jtheberg.fr/CCMC/OS/sc/.NewOS/Prog/ide.lua","ide.lua")
-    run("wget","https://kiz-data.jtheberg.fr/CCMC/OS/sc/.NewOS/Prog/music.lua","music.lua")
-
-    run("cd","/.NewOS/Shell/")
-    run("wget","https://kiz-data.jtheberg.fr/CCMC/OS/sc/.NewOS/Shell/shell.lua","shell.lua")
+    run("cd","/.TelOS/Shell/")
+    run("wget","https://raw.githubusercontent.com/kizYTB/NewOS/refs/heads/main/.TelOS/Shell/shell.lua","shell.lua")
 	
-    run("cd","/.NewOS/pkg/")
+    run("cd","/.TelOS/pkg/")
     run("wget","https://raw.githubusercontent.com/kizYTB/CC-pkg/refs/heads/main/lib-pkg.lua")
 	sleep(2)
-	run("cd","/.NewOS/Up")
-	run("wget","https://kiz-data.jtheberg.fr/CCMC/OS/sc/.NewOS/Up/up.lua","up.lua")
-	run("wget","https://kiz-data.jtheberg.fr/CCMC/OS/sc/.NewOS/Up/install.lua","install.lua")
+	run("cd","/..TelOS/Up")
+	run("wget","https://raw.githubusercontent.com/kizYTB/NewOS/refs/heads/main/.TelOS/Up/up.lua","up.lua")
+	run("wget","https://raw.githubusercontent.com/kizYTB/NewOS/refs/heads/main/.TelOS/Up/install.lua","install.lua")
 end
 
 Bootloader()
